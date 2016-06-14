996bd08c4b32629f788424340a99daf45a2ca633
Wilker Lucio <wilkerlucio@gmail.com>
2016-06-14 17:16:38 -0300

    update figwheel

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src/devcards" "src/cljs"]
+  :source-paths ["src/devcards" "src/cljs" "script"]
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha5" :scope "provided"]
                  [org.clojure/clojurescript "1.9.47" :scope "provided" :exclusions [org.clojure/google-closure-library]]
@@ -14,13 +14,15 @@
                  [navis/untangled-client "0.5.0" :exclusions [org.omcljs/om cljsjs/react-dom org.clojure/tools.reader cljsjs/react]]
 
                  [devcards "0.2.1-4" :exclusions [org.omcljs/om cljsjs/react-dom org.clojure/tools.reader cljsjs/react]]
-                 [binaryage/devtools "0.5.2"]
-                 [figwheel-sidecar "0.5.0-2" :exclusions [clj-time joda-time org.clojure/tools.reader] :scope "test"]
+                 [binaryage/devtools "0.7.0"]
+                 [figwheel-sidecar "0.5.4-3" :exclusions [clj-time joda-time org.clojure/tools.reader] :scope "test"]
                  [org.clojure/test.check "0.9.0"]
 
                  [org.clojure/google-closure-library "0.0-20160609-f42b4a24"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]]
+
+  :figwheel {:open-file-command "open-in-intellij"}
 
   :cljsbuild {
               :builds

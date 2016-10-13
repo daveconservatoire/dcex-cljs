5f1db883b7f29409ea419669b3563f37ecbc6ad4
Wilker Lucio <wilkerlucio@gmail.com>
2016-10-13 14:17:10 -0300

    added dave ex examples

--- src/devcards/daveconservatoire/site/ui/vexflow_cards.cljs
+++ src/devcards/daveconservatoire/site/ui/vexflow_cards.cljs
@@ -1,7 +1,8 @@
 (ns daveconservatoire.site.ui.vexflow-cards
   (:require [devcards.core :refer [defcard deftest dom-node]]
             [cljs.test :refer [is are run-tests async testing]]
-            [daveconservatoire.site.ui.vexflow :as v]))
+            [daveconservatoire.site.ui.vexflow :as v]
+            [om.dom :as dom]))
 
 (defcard blank-score
   (fn [_ _]
@@ -31,16 +32,53 @@
                                     {::v/keys ["f/5"] ::v/duration "q"}]}]}))
   {})
 
-(defcard multi-bar2
+(defcard dave-ex
   (fn [_ _]
-    (v/score {::v/width 500 ::v/height 150
-              ::v/clef ::v/treble
-              ::v/bars [{::v/notes [{::v/keys ["c/5"] ::v/duration "q"}
-                                    {::v/keys ["d/5"] ::v/duration "q"}
-                                    {::v/keys ["e/5"] ::v/duration "q"}
-                                    {::v/keys ["c/4"] ::v/duration "q"}]}
-                        {::v/notes [{::v/keys ["c/5"] ::v/duration "q"}
-                                    {::v/keys ["d/5"] ::v/duration "q"}
-                                    {::v/keys ["e/5"] ::v/duration "q"}
-                                    {::v/keys ["c/4"] ::v/duration "q"}]}]}))
+    (dom/div nil
+      (v/score {::v/width 500 ::v/height 150
+                ::v/clef  ::v/treble
+                ::v/bars  [{::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "h"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "w"}]}]})
+      (v/score {::v/width 500 ::v/height 150
+                ::v/clef  ::v/treble
+                ::v/bars  [{::v/notes [{::v/keys ["b/4"] ::v/duration "w"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "h"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "w"}]}]})
+      (v/score {::v/width 500 ::v/height 150
+                ::v/clef  ::v/treble
+                ::v/bars  [{::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "h"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}]})
+      (v/score {::v/width 500 ::v/height 150
+                ::v/clef  ::v/treble
+                ::v/bars  [{::v/notes [{::v/keys ["b/4"] ::v/duration "w"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "h"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "h"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}
+                                       {::v/keys ["b/4"] ::v/duration "q"}]}
+                           {::v/notes [{::v/keys ["b/4"] ::v/duration "w"}]}]})))
   {})

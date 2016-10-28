4f18169291c53f9b23aca24f5d16b6e7c92314e2
Wilker Lucio <wilkerlucio@gmail.com>
2016-10-28 14:47:31 -0300

    no need to separated load user data on init

--- src/cljs/daveconservatoire/site/core.cljs
+++ src/cljs/daveconservatoire/site/core.cljs
@@ -1,12 +1,9 @@
 (ns daveconservatoire.site.core
   (:require [untangled.client.core :as uc]
             [daveconservatoire.site.routes :as r]
-            [om.next :as om]
-            [untangled.client.data-fetch :as df]
-            [daveconservatoire.site.ui :as ui]))
+            [om.next :as om]))
 
 (defn start-callback [{:keys [reconciler]}]
-  (df/load reconciler :app/me ui/DesktopMenu)
   (om/transact! reconciler `[(app/set-route ~(r/current-handler))]))
 
 (defonce app
